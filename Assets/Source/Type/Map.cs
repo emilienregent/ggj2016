ad4c94fb843374532b458080744a85fff9a4ffb3
Emilien REGENT <emilien.regent@gmail.com>
2016-01-30 18:31:33 +0100

    A Methods to change map environment

--- Assets/Source/Type/Map.cs
+++ Assets/Source/Type/Map.cs
@@ -78,24 +78,79 @@ namespace Type
 
             return indexes[Random.Range(0, indexes.Count)];
         }
+
+        // Retourne toutes les tiles TYPE dans la zone X*Y autour de indexTile
+        // permet de récupérer tous les obstacles dans la zone
+        // permet de récupérer toutes les positions valides dans la zone
+        /*public List<int> GetArea(int indexTile, int columns, int lines, TileType type) {
+            List<int>   indexes = new List<int>();
+            int         tiles   = columns * lines;
+            int         start   = indexTile - Mathf.FloorToInt(lines * 0.5f) * _columns;
+            int         left    = indexTile - Mathf.FloorToInt(columns * 0.5f);
+
+            // Left border not on the same line
+            if(left % _columns > indexTile % _columns)
+                left = 
+
+            // Limit on top-left border of the grid.
+            if (start < 0)
+                start = 0;
+
+
+
+            for (int i = 0; i < lines; i += _columns)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                }
+            }
+
+            return indexes;
+        }*/
+
+        public List<int> GetAllObstacles() {
+            List<int> indexes = new List<int>();
+
+            for (int i = 0; i < _tiles.Count; i++)
+            {
+                Tile tile = _tiles[i];
+
+                if (tile.type >= (int)TileType.OBSTACLE_1)
+                {
+                    indexes.Add(i);
+                }
+            }
+
+            return indexes;
+        }
+        
+        public void TransformTile(int indexTile, TileType type) {
+            if (indexTile < _tiles.Count)
+            {
+                _tiles[indexTile].type = (int)type;
+            }
+        }
+        
+        public void MoveAltar(int indexTile) {
+            int currentIndexOfAltar = 0;
+
+            for (int i = 0; i < _tiles.Count; i++)
+            {
+                Tile tile = _tiles[i];
+
+                if (tile.type == (int)TileType.ALTAR)
+                {
+                    currentIndexOfAltar = i;
+                    break;
+                }
+            }
+
+            if (indexTile < _tiles.Count)
+            {
+                _tiles[indexTile].type = (int)TileType.ALTAR;
+                _tiles[currentIndexOfAltar].type = (int)TileType.DEFAULT;
+            }
+
+        }
     }
-
-//	public List<int> GetTileInArea(int indexTile, int x, int y, TileType type) {
-//		// Retourne toutes les tiles TYPE dans la zone X*Y autour de indexTile
-//		// permet de récupérer tous les obstacles dans la zone
-//		// permet de récupérer toutes les positions valides dans la zone
-//	}
-
-//	public List<int> GetAllObstacles() {
-//		// Retourne toutes les positions des obstacles
-//	}
-
-//	public void TransformTile(int indexTile, TileType type) {
-//		// Transforme la tile à la position indexTile en tile de type type
-//	}
-
-//	public void MoveAltar(int indexTile) {
-//		// Déplace l'autel à la position indexTile
-//	}
-
 }
