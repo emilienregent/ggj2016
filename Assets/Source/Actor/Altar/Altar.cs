d4b259183a7beb98773a97ed5abb21ab0c8b8427
Emilien REGENT <emilien.regent@gmail.com>
2016-01-31 12:57:24 +0100

    M Use custom event with timer to activate altar

--- Assets/Source/Actor/Altar/Altar.cs
+++ Assets/Source/Actor/Altar/Altar.cs
@@ -3,15 +3,30 @@ using UnityEngine;
 
 public class Altar : MonoBehaviour
 {
-    public void OnActivate()
+    private Timer   _timer          = new Timer(0f);
+    private bool    _isActivated    = false;
+
+    public void OnShow()
     {
-        Game.instance.currentPlayer.score += Game.instance.currentPlayer.getPointsMinions();
-        Game.instance.currentPlayer.portrait.UpdateScore(Game.instance.currentPlayer.score);
-        Game.instance.currentPlayer.KillAllMinions();
+        _isActivated = true;
+        _timer.duration = 0.25f;
+        _timer.Start();
     }
 
     public void OnHide()
     {
         Game.instance.EndTurn();
+    }
+
+    private void Update()
+    {
+        if (_isActivated == true && _timer.IsFinished() == true)
+        {
+            _isActivated = false;
+
+            Game.instance.currentPlayer.score += Game.instance.currentPlayer.getPointsMinions();
+            Game.instance.currentPlayer.portrait.UpdateScore(Game.instance.currentPlayer.score);
+            Game.instance.currentPlayer.KillAllMinions();
+        }
     }
 }
