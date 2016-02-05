37a5bb637fcd72a7a1f67bcd1d40c22b3a0a8b0f
Emilien REGENT <e.regent@owlient.eu>
2016-02-05 14:41:17 +0100

    M Fix respawn as design
    Change-Id: I23a77fc96cee32b35b428a7a1c04740fe1da87fe

--- Assets/Source/Game/Game.cs
+++ Assets/Source/Game/Game.cs
@@ -175,15 +175,11 @@ public class Game : MonoBehaviour
 
         if (_numberOfTurn % 2 == 0)
         {
-            if (++_numberOfTurnSinceRespawn % GameConfiguration.TURN_BEFORE_RESPAWN == 0 && Minion.number < GameConfiguration.NUMBER_OF_MINION_TRESHOLD)
+            if (Minion.number < GameConfiguration.NUMBER_OF_MINION_TRESHOLD && ++_numberOfTurnSinceRespawn >= GameConfiguration.TURN_BEFORE_RESPAWN)
             {
                 int index = mapManager.map.GetRandomAvailableIndex();
 
                 mapManager.map.TransformTile(index, (TileType)Random.Range((int)TileType.MINION_GREEN, (int)TileType.MINION_YELLOW));
-            }
-            else if (Minion.number >= GameConfiguration.NUMBER_OF_MINION_TRESHOLD)
-            {
-                _numberOfTurnSinceRespawn = 0;
             }
         }
 
